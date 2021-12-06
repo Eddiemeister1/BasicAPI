68aadb089949306530d141f6fbaefda43c75b7e0
eduardo dotel <eduardodotel@yahoo.com>
2021-12-06 15:39:33 -0500

    Added Data folder with data context

---
+++ Data/BasicDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicAPI.Data
+{
+    public class BasicDataContext : DbContext
+    {
+        public BasicDataContext(DbContextOptions<BasicDataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Agent>? Agents { get; set; }
+    }
+
+}
